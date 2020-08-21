45b44310e01994f91a6642850cf3b1536fb0a5c3
Bruno Santos <brunosansp@outlook.com>
2020-08-20 23:34:29 -0300

    add repository and crud methods

--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,10 +1,26 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
 public class Cliente {
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull
 	private String nome;
+	
+	@NotNull
 	private String email;
+	
+	@Column(name = "telefone")
 	private String telefone;
 	public Long getId() {
 		return id;
@@ -30,5 +46,30 @@ public class Cliente {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
